e76c74a172a52af6c5ce165e2571bdb6bf8bbbae
dstriker12 <dbrodsky@students.kennesaw.edu>
2020-03-02 20:49:55 -0500

    Functional Requirements for 2 and 3

--- Verification Tests/UnitTest1.cs
+++ Verification Tests/UnitTest1.cs
@@ -1,15 +1,99 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 
 
 namespace Verification_Tests
 {
-    [TestClass]
+    [TestFixture]
     public class Verification
     {
-        [TestMethod]
-        public void TestMethod1()
+        [Test]
+        public void firstLocationTest()
         {
+            Location loc1 = new Location();
+
+            Assert.IsNotNull(loc1);
+        }
+
+        [Test]
+        public void secondLocationTest()
+        {
+            Location loc2 = new Location();
+
+            Assert.IsNotNull(loc2);
+        }
+
+        [Test]
+        public void routeTest()
+        {
+            Route r1 = new Route();
+            Route r2 = new Route();
+
+            Assert.AreNotEqual(r1.path(),r2.path());
+        }
+
+        [Test]
+        public void TrafficPatternTest()
+        {
+            string flow = "";
+
+            Traffic t1 = new Traffic();
+
+            Assert.AreEqual(t1.Pattern(), flow);
+        }
+
+        [Test]
+        public void BestRouteTest()
+        {
+            Route r1 = new Route();
+            Route r2 = new Route();
+
+            if (r1.travelTime() < r2.travelTime())
+                Assert.Greater(r2.travelTime(), r1.travelTime());
+            else
+                Assert.Greater(r1.travelTime(), r2.travelTime());
+
+        }
+
+        [Test]
+        public void trafficConditionsTest()
+        {
+            Route r1 = new Route();
+            string testCondition = "";
+
+            Assert.Equals(r1.condition(), testCondition);
+        }
+    }
+
+    internal class Location
+    {
+    }
+
+    internal class Route
+    {
+
+        public Route path()
+        {
+            return null;
+        }
+
+        public double travelTime()
+        {
+            return 0.0;
+        }
+
+        public string condition()
+        {
+            return "";
+        }
+    }
+
+    public class Traffic
+    {
+        public string Pattern()
+        {
+            return null;
         }
     }
 }
